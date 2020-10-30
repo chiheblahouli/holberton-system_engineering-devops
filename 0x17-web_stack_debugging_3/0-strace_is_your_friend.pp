ab3d630c0c7f2581a60c905b86da26fc19ea529b
chiheblahouli <chiheb.lahouli@gmail.com>
2020-10-30 10:13:42 +0100

    c

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
 #fix the errors
 
-exec { 'fix':
-  command  => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+exec { 'fix typo':
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
