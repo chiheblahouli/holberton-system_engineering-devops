c5a6d11e041d9e134d558298def260b7035b8931
chiheblahouli <chiheb.lahouli@gmail.com>
2020-10-30 10:23:19 +0100

    c

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,7 @@
 #use puppet
 
 exec { 'fix':
-  command  => 'sudo sed -i "s/15/4096/g" /etc/default/nginx restart',
+  onlyif  => 'test -e /etc/default/nginx',
+  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
