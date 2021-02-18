c508c64315c81f6d03524f8275258b9ac7161e0c
chiheblahouli <chiheb.lahouli@gmail.com>
2021-02-18 13:12:04 +0100

    c

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,14 +7,14 @@ import requests
 
 
 def top_ten(subreddit):
-
-    if subreddit is None or type(subreddit) is not str:
-        print(None)
+    header = {'User-Agent': 'fake'}
+    param = {'limit': 10}
     r = requests.get('http://www.reddit.com/r/{}/hot.json'.format(subreddit),
                      headers=header,
                      params=param)
-    if posts is None or (len(posts) > 0 and posts[0].get('kind') != 't3'):
-        print(None)
-    else:
+    if r.status_code == requests.codes.ok:
+        posts = r.json().get('data').get('children')
         for post in posts:
-            print(post.get('data', {}).get('title', None))
+            print(post.get('data', {}).get('title', ''))
+    else:
+        print(None)
